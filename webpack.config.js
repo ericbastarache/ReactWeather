628593215d3e3936bd5b699fb3213b06fe3709bf
ericb <eric_bastarache@hotmail.com>
2016-12-12 17:51:58 -0500

    webpack configuration file

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var HtmlWebPackPlugin = require('html-webpack-plugin');
+var HTMLWebpackPluginConfig = new HtmlWebPackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = {
+  entry: [
+    './app/index.js'
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+    ]
+  },
+  plugins: [HTMLWebpackPluginConfig]
+}
